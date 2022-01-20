e80050b12277fd62445f41048722b5394edd56bd
Robiul <victorshakil9102@gmail.com>
2022-01-20 22:31:19 +0600

    update

--- Assignment-3/TicketBookingSystem.Data/UnitOfWork.cs
+++ Assignment-3/TicketBookingSystem.Data/UnitOfWork.cs
@@ -10,15 +10,15 @@ namespace TicketBookingSystem.Data
     {
         protected readonly DbContext _dbContext;
 
-        public UnitOfWork(DbContext dbContext) => this._dbContext = dbContext;
+        public UnitOfWork(DbContext dbContext) => _dbContext = dbContext;
 
-        public void Dispose() => this._dbContext?.Dispose();
+        public void Dispose() => _dbContext?.Dispose();
 
-        public void Save() => this._dbContext?.SaveChanges();
+        public void Save() => _dbContext?.SaveChanges();
 
         public async Task SaveAsync()
         {
-            int num = await this._dbContext.SaveChangesAsync();
+            int num = await _dbContext.SaveChangesAsync();
         }
     }
 }
